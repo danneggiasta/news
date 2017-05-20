0b8f825f906cd3b616a3679da0c71a7c49811377
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-05-20 16:11:36 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -79,14 +79,7 @@ session_write_close();
                 <h3>Phasellus lorem erat, fringilla vestibulum est sit amet, lobortis tincidunt leo. Nam non
                     posuere lorem.</h3>
 
-                <p>Nulla vitae ultricies metus, sed consequat elit. Suspendisse nisl velit, cursus sed mattis a, varius
-                    sit amet nibh. Cras imperdiet nulla vitae euismod blandit. Sed pulvinar nibh ut dolor semper
-                    accumsan. Phasellus lorem erat, fringilla vestibulum est sit amet, lobortis tincidunt leo. Nam non
-                    posuere lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
-                    egestas. Nunc scelerisque, odio eu efficitur commodo, tortor tellus accumsan magna, ut eleifend sem
-                    velit vitae sapien. Donec nec ornare mi, sit amet mollis neque. Donec mattis sapien elit, at
-                    hendrerit diam suscipit in. In molestie ut diam vitae luctus. Duis neque lorem, ultricies eu elit
-                    vitae, malesuada varius arcu. Suspendisse lobortis blandit orci sit amet mattis.</p>
+                <p><?php if(isset($page->output['body'])) { echo $page->output['body']; } ?></p>
 
             </div>
 

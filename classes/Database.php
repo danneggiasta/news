ff8cdaa85ff3930dfe3b78c010e181fd7351f3e5
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-05-22 11:42:12 +0200

    Add files via upload

--- classes/Database.php
+++ classes/Database.php
@@ -1,44 +1,50 @@
 <?php
-
 include 'config/database.php';
 
 Class Database
 {
-
     private $server = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . "";
-
     private $user = DB_USER;
-
     private $pass = DB_PASS;
 
     private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 
-    protected $con;
+    private $db;
 
-    public function openConnection()
+    private static $instance = null;
 
+    private function __construct()
     {
         try {
+            $this->db = new PDO($this->server, $this->user, $this->pass, $this->options);
 
-            $this->con = new PDO($this->server, $this->user, $this->pass, $this->options);
-
-            return $this->con;
+            return $this->db;
 
         } catch (PDOException $e) {
 
             return "There is some problem in connection: " . $e->getMessage();
+        }
+    }
 
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+
+            self::$instance = new Database();
         }
 
+        return self::$instance;
     }
 
     public function closeConnection()
     {
-
-        $this->con = null;
-
+        $this->db = null;
     }
 
+    public function query($sql){
+
+        return $this->db->query();
+    }
 }
 
 ?>
